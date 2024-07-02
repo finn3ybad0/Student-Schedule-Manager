d6630123e580708ec63a3547c29cf82079d5b56a
Finney Bado <finney.bado@icloud.com>
2024-07-02 19:19:50 -0400

    folder structure adjustment

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.studentschedulemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.studentschedulemanager to javafx.fxml;
+    exports com.example.studentschedulemanager;
+}
